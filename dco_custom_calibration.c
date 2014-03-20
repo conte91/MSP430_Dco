83779459dc21cd4d30d952d9ad31b61bee7e4f6e
Simone Baratta <Conte91@gmail.com>
2014-03-20 21:56:15 +0100

    DCOcal uses defines

---
+++ dco_custom_calibration.c
@@ -0,0 +1,136 @@
+#include <msp430.h>
+#include "dco_calibration.h"
+
+void Setup_DCO(DCOclock which){
+
+  switch(which){
+    case DCO_1MHZ:
+      if (CALBC1_1MHZ ==0xFF || CALDCO_1MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_1MHZ;
+      BCSCTL1=CALBC1_1MHZ;
+      break;
+    case DCO_2MHZ:
+      if (CALBC1_2MHZ ==0xFF || CALDCO_2MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_2MHZ;
+      BCSCTL1=CALBC1_2MHZ;
+      break;
+    case DCO_3MHZ:
+      if (CALBC1_3MHZ ==0xFF || CALDCO_3MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_3MHZ;
+      BCSCTL1=CALBC1_3MHZ;
+      break;
+    case DCO_4MHZ:
+      if (CALBC1_4MHZ ==0xFF || CALDCO_4MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_4MHZ;
+      BCSCTL1=CALBC1_4MHZ;
+      break;
+    case DCO_5MHZ:
+      if (CALBC1_5MHZ ==0xFF || CALDCO_5MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_5MHZ;
+      BCSCTL1=CALBC1_5MHZ;
+      break;
+    case DCO_6MHZ:
+      if (CALBC1_6MHZ ==0xFF || CALDCO_6MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_6MHZ;
+      BCSCTL1=CALBC1_6MHZ;
+      break;
+    case DCO_7MHZ:
+      if (CALBC1_7MHZ ==0xFF || CALDCO_7MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_7MHZ;
+      BCSCTL1=CALBC1_7MHZ;
+      break;
+    case DCO_8MHZ:
+      if (CALBC1_8MHZ ==0xFF || CALDCO_8MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_8MHZ;
+      BCSCTL1=CALBC1_8MHZ;
+      break;
+    case DCO_9MHZ:
+      if (CALBC1_9MHZ ==0xFF || CALDCO_9MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_9MHZ;
+      BCSCTL1=CALBC1_9MHZ;
+      break;
+    case DCO_10MHZ:
+      if (CALBC1_10MHZ ==0xFF || CALDCO_10MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_10MHZ;
+      BCSCTL1=CALBC1_10MHZ;
+      break;
+    case DCO_11MHZ:
+      if (CALBC1_11MHZ ==0xFF || CALDCO_11MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_11MHZ;
+      BCSCTL1=CALBC1_11MHZ;
+      break;
+    case DCO_12MHZ:
+      if (CALBC1_12MHZ ==0xFF || CALDCO_12MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_12MHZ;
+      BCSCTL1=CALBC1_12MHZ;
+      break;
+    case DCO_13MHZ:
+      if (CALBC1_13MHZ ==0xFF || CALDCO_13MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_13MHZ;
+      BCSCTL1=CALBC1_13MHZ;
+      break;
+    case DCO_14MHZ:
+      if (CALBC1_14MHZ ==0xFF || CALDCO_14MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_14MHZ;
+      BCSCTL1=CALBC1_14MHZ;
+      break;
+    case DCO_15MHZ:
+      if (CALBC1_15MHZ ==0xFF || CALDCO_15MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_15MHZ;
+      BCSCTL1=CALBC1_15MHZ;
+      break;
+    case DCO_16MHZ:
+      if (CALBC1_16MHZ ==0xFF || CALDCO_16MHZ == 0xFF) 	// If calibration constants erased                                     
+  		{
+  			while(1);                   				// do not load, trap CPU!
+  		}
+      DCOCTL=CALDCO_16MHZ;
+      BCSCTL1=CALBC1_16MHZ;
+      break;
+  }
+}
--- dco_custom_calibration.h
+++ dco_custom_calibration.h
@@ -1,73 +1,57 @@
-// Custom calibration of msp430's DCO
-// include this file in your project after
-// flash costants calibration with dco_cal project http://
-// Michele Porelli & Massimo Pennazio 2012
-// Creative commons license
-//
-#ifndef DCO_CUSTOM_CALIBRATION_H_
-	#define DCO_CUSTOM_CALIBRATION_H_
+#pragma once
 
-	#ifndef __DisableCalData
-		#define __DisableCalData		// directive used to disable factory calibration
-			#ifdef CALDCO_16MHZ
-				#undef CALDCO_16MHZ
-			#endif
-			#ifdef CALBC1_16MHZ
-				#undef CALBC1_16MHZ
-			#endif
-			#ifdef CALDCO_12MHZ
-				#undef CALDCO_12MHZ
-			#endif
-			#ifdef CALBC1_12MHZ
-				#undef CALBC1_12MHZ
-			#endif
-			#ifdef CALDCO_8MHZ
-				#undef CALDCO_8MHZ
-			#endif
-			#ifdef CALBC1_8MHZ
-				#undef CALBC1_8MHZ
-			#endif
-			#ifdef CALDCO_1MHZ
-				#undef CALDCO_1MHZ
-			#endif
-			#ifdef CALBC1_1MHZ
-				#undef CALBC1_1MHZ
-			#endif
-			
-		#define __CustomCalData			// instead use ad hoc calibrated frequecies
-		
-		SFR_8BIT(CALDCO_16MHZ);                       /* DCOCTL  Calibration Data for 16MHz */
-		SFR_8BIT(CALBC1_16MHZ);                       /* BCSCTL1 Calibration Data for 16MHz */
-		SFR_8BIT(CALDCO_15MHZ);                       /* DCOCTL  Calibration Data for 15MHz */
-		SFR_8BIT(CALBC1_15MHZ);                       /* BCSCTL1 Calibration Data for 15MHz */
-		SFR_8BIT(CALDCO_14MHZ);                        /* DCOCTL  Calibration Data for 14MHz */
-		SFR_8BIT(CALBC1_14MHZ);                        /* BCSCTL1 Calibration Data for 14MHz */
-		SFR_8BIT(CALDCO_13MHZ);                        /* DCOCTL  Calibration Data for 13MHz */
-		SFR_8BIT(CALBC1_13MHZ);                        /* BCSCTL1 Calibration Data for 13MHz */
-		SFR_8BIT(CALDCO_12MHZ);                       /* DCOCTL  Calibration Data for 12MHz */
-		SFR_8BIT(CALBC1_12MHZ);                       /* BCSCTL1 Calibration Data for 12MHz */
-		SFR_8BIT(CALDCO_11MHZ);                       /* DCOCTL  Calibration Data for 11MHz */
-		SFR_8BIT(CALBC1_11MHZ);                       /* BCSCTL1 Calibration Data for 11MHz */
-		SFR_8BIT(CALDCO_10MHZ);                        /* DCOCTL  Calibration Data for 10MHz */
-		SFR_8BIT(CALBC1_10MHZ);                        /* BCSCTL1 Calibration Data for 10MHz */
-		SFR_8BIT(CALDCO_9MHZ);                        /* DCOCTL  Calibration Data for 9MHz */
-		SFR_8BIT(CALBC1_9MHZ);                        /* BCSCTL1 Calibration Data for 9MHz */
-		SFR_8BIT(CALDCO_8MHZ);                       /* DCOCTL  Calibration Data for 8MHz */
-		SFR_8BIT(CALBC1_8MHZ);                       /* BCSCTL1 Calibration Data for 8MHz */
-		SFR_8BIT(CALDCO_7MHZ);                       /* DCOCTL  Calibration Data for 7MHz */
-		SFR_8BIT(CALBC1_7MHZ);                       /* BCSCTL1 Calibration Data for 7MHz */
-		SFR_8BIT(CALDCO_6MHZ);                        /* DCOCTL  Calibration Data for 6MHz */
-		SFR_8BIT(CALBC1_6MHZ);                        /* BCSCTL1 Calibration Data for 6MHz */
-		SFR_8BIT(CALDCO_5MHZ);                        /* DCOCTL  Calibration Data for 5MHz */
-		SFR_8BIT(CALBC1_5MHZ);                        /* BCSCTL1 Calibration Data for 5MHz */
-		SFR_8BIT(CALDCO_4MHZ);                       /* DCOCTL  Calibration Data for 4MHz */
-		SFR_8BIT(CALBC1_4MHZ);                       /* BCSCTL1 Calibration Data for 4MHz */
-		SFR_8BIT(CALDCO_3MHZ);                       /* DCOCTL  Calibration Data for 3MHz */
-		SFR_8BIT(CALBC1_3MHZ);                       /* BCSCTL1 Calibration Data for 3MHz */
-		SFR_8BIT(CALDCO_2MHZ);                        /* DCOCTL  Calibration Data for 2MHz */
-		SFR_8BIT(CALBC1_2MHZ);                        /* BCSCTL1 Calibration Data for 2MHz */
-		SFR_8BIT(CALDCO_1MHZ);                        /* DCOCTL  Calibration Data for 1MHz */
-		SFR_8BIT(CALBC1_1MHZ);                        /* BCSCTL1 Calibration Data for 1MHz */
+#define CALDCO_1MHZ  (*((unsigned char*) 0x10C0))
+#define CALBC1_1MHZ  (*((unsigned char*) 0x10C1))
+#define CALDCO_2MHZ  (*((unsigned char*) 0x10C2))
+#define CALBC1_2MHZ  (*((unsigned char*) 0x10C3))
+#define CALDCO_3MHZ  (*((unsigned char*) 0x10C4))
+#define CALBC1_3MHZ  (*((unsigned char*) 0x10C5))
+#define CALDCO_4MHZ  (*((unsigned char*) 0x10C6))
+#define CALBC1_4MHZ  (*((unsigned char*) 0x10C7))
+#define CALDCO_5MHZ  (*((unsigned char*) 0x10C8))
+#define CALBC1_5MHZ  (*((unsigned char*) 0x10C9))
+#define CALDCO_6MHZ  (*((unsigned char*) 0x10CA))
+#define CALBC1_6MHZ  (*((unsigned char*) 0x10CB))
+#define CALDCO_7MHZ  (*((unsigned char*) 0x10CC))
+#define CALBC1_7MHZ  (*((unsigned char*) 0x10CD))
+#define CALDCO_8MHZ  (*((unsigned char*) 0x10CE))
+#define CALBC1_8MHZ  (*((unsigned char*) 0x10CF))
+#define CALDCO_9MHZ  (*((unsigned char*) 0x10D0))
+#define CALBC1_9MHZ  (*((unsigned char*) 0x10D1))
+#define CALDCO_10MHZ (*((unsigned char*) 0x10D2))
+#define CALBC1_10MHZ (*((unsigned char*) 0x10D3))
+#define CALDCO_11MHZ (*((unsigned char*) 0x10D4))
+#define CALBC1_11MHZ (*((unsigned char*) 0x10D5))
+#define CALDCO_12MHZ (*((unsigned char*) 0x10D6))
+#define CALBC1_12MHZ (*((unsigned char*) 0x10D7))
+#define CALDCO_13MHZ (*((unsigned char*) 0x10D8))
+#define CALBC1_13MHZ (*((unsigned char*) 0x10D9))
+#define CALDCO_14MHZ (*((unsigned char*) 0x10F0))
+#define CALBC1_14MHZ (*((unsigned char*) 0x10F1))
+#define CALDCO_15MHZ (*((unsigned char*) 0x10F2))
+#define CALBC1_15MHZ (*((unsigned char*) 0x10F3))
+#define CALDCO_16MHZ (*((unsigned char*) 0x10F4))
+#define CALBC1_16MHZ (*((unsigned char*) 0x10F5))
 
-	#endif
-#endif /*DCO_CUSTOM_CALIBRATION_H_*/
+enum _DCOclock{
+    DCO_1MHZ,
+    DCO_2MHZ,
+    DCO_3MHZ,
+    DCO_4MHZ,
+    DCO_5MHZ,
+    DCO_6MHZ,
+    DCO_7MHZ,
+    DCO_8MHZ,
+    DCO_9MHZ,
+    DCO_10MHZ,
+    DCO_11MHZ,
+    DCO_12MHZ,
+    DCO_13MHZ,
+    DCO_14MHZ,
+    DCO_15MHZ,
+    DCO_16MHZ
+    };
+
+typedef enum _DCOclock DCOclock;
+
+void Setup_DCO(DCOclock which);
